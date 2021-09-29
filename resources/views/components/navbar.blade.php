4ace0c7f77f136486be5335ac3d1e3529e78468b
Olexander <abbos.t.2001@gmail.com>
2021-09-30 00:36:40 +0500

    lang_color

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,7 +21,7 @@
                     <div class="dropdown-menu " aria-labelledby="">
                         @foreach (Config::get('languages') as $lang => $language)
                             @if ($lang != App::getLocale())
-                                <a class="dropdown-item text-white " href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
+                                <a class="dropdown-item " href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
                             @endif
                         @endforeach
                     </div>

0177e01b9a53f6a8e5468a20ae41ab26c43391e3
maxwell <shaxa882@gmail.com>
2021-09-29 23:50:19 +0500

    new changes

--- app/View/Components/Carousel.php
+++ app/View/Components/Carousel.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class carousel extends Component
+class Carousel extends Component
 {
     /**
      * Create a new component instance.

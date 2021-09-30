40495253572c195041c82a4f6d9bed3df65e39b7
Olexander <abbos.t.2001@gmail.com>
2021-10-01 00:38:55 +0500

    lang button

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,10 +1,10 @@
 <section class="pb_cover_v1 text-left cover-bg-black cover-bg-opacity-4 justify-content-center" style="background-image: url({{$image_banner->image}})" id="section-home">
     <div class="container">
-        <div class="row align-items-center">
+        <div class="row align-items-center justify-content-center">
             <div class="col-md-6  order-md-1 ">
-                <h2 class="heading mb-3 font-weight-boldsub-heading ">WHY MUSIC PRO</h2>
-                <div class=""><p class="mb-5 "><h1 class="text-white">{{__('messages.banner')}}</h1></p>
-                    <p><a href="tel:909751090" role="button" class="btn smoothscroll pb_outline-light btn-xl pb_font-13 p-4 rounded-0 pb_letter-spacing-2">{{__('messages.consulting')}}</a></p>
+                <h2 class="heading mb-3 font-weight-boldsub-heading text-center">WHY MUSIC PRO</h2>
+                <div class=""><h2 class="text-white text-center">{{__('messages.banner')}}</h2></p>
+                    <p class="text-center"><a href="tel:909751090" role="button" class="btn smoothscroll pb_outline-light btn-xl pb_font-13 p-4 rounded-0 pb_letter-spacing-2">{{__('messages.consulting')}}</a></p>
                 </div>
             </div>
         </div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,32 +1,43 @@
 <nav class="navbar navbar-expand-lg navbar-dark pb_navbar pb_scrolled-light bg-gradient-light" id="pb-navbar">
-    <div class="container">
-        <a class="navbar-brand image-logo" href="/"><img style="width: 200px"  src="{{$image_logo->image}}" alt=""></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#probootstrap-navbar" aria-controls="probootstrap-navbar" aria-expanded="false" aria-label="Toggle navigation">
+    <ul class="container">
+        <a class="navbar-brand image-logo" href="/"><img style="width: 200px" src="{{$image_logo->image}}" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#probootstrap-navbar"
+                aria-controls="probootstrap-navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span><i class="ion-navicon"></i></span>
         </button>
         <div class="collapse navbar-collapse" id="probootstrap-navbar">
             <ul class="navbar-nav ml-auto">
                 @foreach($navigation as $navitem)
-                <li class="nav-item"><a class="nav-link" href="#section-{{$navitem->section}}">{{$navitem->nav}}</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#section-{{$navitem->section}}">{{$navitem->nav}}</a>
+                    </li>
                 @endforeach
-                    <div>
-                        <div class="flex justify-center pt-8 sm:justify-start sm:pt-0 ">
-                            <li class="navbar-nav ml-auto dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
-                                    {{ Config::get('languages')[App::getLocale()] }}
-                                </a>
-                                <div class="dropdown-menu " aria-labelledby="">
-                                    @foreach (Config::get('languages') as $lang => $language)
-                                        @if ($lang != App::getLocale())
-                                            <a class="dropdown-item " href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
-                                        @endif
-                                    @endforeach
-                                </div>
-                            </li>
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0 ">
+                    <li>
+
+                    <li class=" ml-auto dropdown">
+                        <a class="dropdown-toggle text-white" href="#" id="" data-toggle="dropdown"
+                           aria-haspopup="false"
+                           aria-expanded="false">
+                            {{ Config::get('languages')[App::getLocale()] }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="" style="background-color: transparent">
+                            @foreach (Config::get('languages') as $lang => $language)
+                                @if ($lang != App::getLocale())
+                                    <a class="dropdown-item text-white"
+                                       href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
+                                @endif
+                            @endforeach
                         </div>
-                    </div>
+                    </li>
+                </div>
+                </li>
             </ul>
         </div>
-    </div>
+        <div>
+
+        </div>
+
+
+        </div>
 </nav>
 <!-- END nav -->
